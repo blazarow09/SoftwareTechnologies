6cfcd368cb262045a2a9eb3398bf88c5b3083231
blazarow09 <blazarow09@gmail.com>
2018-07-23 02:59:30 +0300

    uploading Csharp directory

---
+++ CSharp/NumbersFrom1To50/NumbersFrom1To50/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace NumbersFrom1To50.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
