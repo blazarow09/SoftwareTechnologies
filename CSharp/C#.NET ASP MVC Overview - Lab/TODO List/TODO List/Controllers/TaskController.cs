b17bfdb395fae2a17f2c2990bdabb322d473c87b
blazarow09 <blazarow09@gmail.com>
2018-07-23 15:42:32 +0300

    added Edit method

--- CSharp/C#.NET ASP MVC Overview - Lab/TODO List/TODO List/Controllers/TaskController.cs
+++ CSharp/C#.NET ASP MVC Overview - Lab/TODO List/TODO List/Controllers/TaskController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using TODOList.Models;
 
 namespace TODO_List.Controllers
@@ -6,7 +7,7 @@ namespace TODO_List.Controllers
     public class TaskController : Controller
     {
         [HttpPost]
-        public IActionResult Create( Task task)
+        public IActionResult Create(Task task)
         {
             if (task == null)
             {
@@ -44,6 +45,63 @@ namespace TODO_List.Controllers
             }
 
             return RedirectToAction("Index", "Home");
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int? id)
+        {
+
+            if (id == null)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            using (var db = new TaskDbContext())
+            {
+                var task = db.Tasks.Find(id);
+
+
+
+                if (task == null)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+
+                return View(task);
+
+            }
+
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Task task)
+        {
+            using (var db = new TaskDbContext())
+            {
+                if (ModelState.IsValid)
+                {
+                    db.Entry(task).State = EntityState.Modified;
+                    db.SaveChanges();
+                }
+
+                return RedirectToAction("Index", "Home");
+            }
+        }
+
+
+        [HttpGet]
+        public ActionResult Details(int? id)
+        {
+            using (var db = new TaskDbContext())
+            {
+                var task = db.Tasks.Find(id);
+
+                var movie = task;
+
+                return View(movie);
+                    
+            }
+
         }
     }
 }
