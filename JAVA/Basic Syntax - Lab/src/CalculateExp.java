9807817b910e93e0df1215299dd85a856a511081
blazarow09 <blazarow09@gmail.com>
2018-07-30 00:58:21 +0300

    uploading Java Basic Syntax - Lab dir

---
+++ JAVA/Basic Syntax - Lab/src/CalculateExp.java
@@ -0,0 +1,8 @@
+public class CalculateExp {
+    public static void main (String[] args) {
+
+        double val = ((30 + 21) * 0.5 * (35 - 12 - 0.5));
+        double eVal = Math.pow(val , 2);
+        System.out.print(eVal);
+    }
+}
