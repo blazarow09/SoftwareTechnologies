7c15d11f8bbfd3cea63ddc9cbde1fc5f885eb755
blazarow09 <blazarow09@gmail.com>
2018-07-30 02:13:56 +0300

    up BooleanVariable ex

---
+++ JAVA/BasicSyntax - Exercise/src/BooleanVariable.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class BooleanVariable {
+    public static void main(String[] args){
+
+        Scanner scan = new Scanner(System.in);
+
+        boolean isTrue = Boolean.parseBoolean(scan.nextLine());
+
+        System.out.println(isTrue ? "Yes" : "No");
+    }
+}
