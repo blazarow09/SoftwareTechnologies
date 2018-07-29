795bddab65e2f545bcf5fd7c88c54cf1e1603770
blazarow09 <blazarow09@gmail.com>
2018-07-30 01:24:54 +0300

    up ThreeIntegersSum

--- JAVA/Basic Syntax - Lab/src/ThreeIntegersSum.java
+++ JAVA/Basic Syntax - Lab/src/ThreeIntegersSum.java
@@ -1,5 +1,38 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class ThreeIntegersSum {
     public static void main(String[] args){
-        
+
+        Scanner scan = new Scanner(System.in);
+
+        int[] numbers = Arrays.stream(scan.nextLine().split(" "))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+
+        int firstNum = numbers[0];
+        int secondNum = numbers[1];
+        int thirdNum = numbers[2];
+
+        if (!FindThreeIntSum(firstNum, secondNum, thirdNum)
+                && !FindThreeIntSum(firstNum, thirdNum, secondNum)
+                &&!FindThreeIntSum(secondNum, thirdNum, firstNum)) {
+            System.out.println("No");
+        }
     }
+
+    private static boolean FindThreeIntSum(int firstNum,int secondNum, int thirdNum) {
+
+        if (firstNum + secondNum != thirdNum){
+            return false;
+        }
+
+        int smallNum = Math.min(firstNum, secondNum);
+        int bigestNum = Math.max(firstNum, secondNum);
+
+        System.out.printf("%d + %d = %d%n", smallNum, bigestNum, thirdNum);
+
+        return true;
+    }
+
 }
