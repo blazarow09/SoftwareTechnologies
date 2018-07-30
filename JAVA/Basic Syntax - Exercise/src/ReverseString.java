b95381292da808ce942fc103d303a86fbc572c9f
blazarow09 <blazarow09@gmail.com>
2018-07-30 15:09:52 +0300

    upload ReverseString exercise

---
+++ JAVA/Basic Syntax - Exercise/src/ReverseString.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class ReverseString {
+    public static void main(String[] args){
+
+        Scanner scanner = new Scanner(System.in);
+
+        String line = scanner.nextLine();
+
+        for (int i = line.length() - 1;i >= 0; i--){
+            System.out.print(line.charAt(i));
+        }
+    }
+}
