89b8fc69c67f8a06d11bfaea7a15c0b17f9f7332
blazarow09 <blazarow09@gmail.com>
2018-08-12 16:23:01 +0300

    upload IssueTracker exam 12.08.2018 C#

---
+++ CSharp/ExamPreparation/IssueTracker/IssueTracker/Controllers/IssueController.cs
@@ -0,0 +1,91 @@
+﻿namespace IssueTracker.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+    using IssueTracker.Models;
+    using System.Linq;
+    using System.Collections.Generic;
+
+    public class IssueController : Controller
+    {
+        private readonly IssueDbContext context;
+
+        public IssueController(IssueDbContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        [Route("")]
+        public ActionResult Index()
+        {
+            List<Issue> issues = this.context.Issues.ToList();
+            return View(issues);
+        }
+
+        [HttpGet]
+        [Route("create")]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("create")]
+        public ActionResult Create(Issue issue)
+        {
+           if (ModelState.IsValid)
+            {
+                this.context.Issues.Add(issue);
+                this.context.SaveChanges();
+
+                return Redirect("/");
+            }
+            return View();
+        }
+
+        [HttpGet]
+        [Route("edit/{id}")]
+        public ActionResult Edit(int id)
+        {
+            var issue = this.context.Issues.Find(id);
+
+            return View(issue);
+        }
+
+        [HttpPost]
+        [Route("edit/{id}")]
+        [ValidateAntiForgeryToken]
+        public ActionResult EditConfirm(int id, Issue issueModel)
+        {
+            if (ModelState.IsValid)
+            {
+                this.context.Issues.Update(issueModel);
+                this.context.SaveChanges();
+
+                return Redirect("/");
+            }
+
+            return View(issueModel);
+        }
+
+        [HttpGet]
+        [Route("delete/{id}")]
+        public ActionResult Delete(int id)
+        {
+            var issue = this.context.Issues.Find(id);
+
+            return View(issue);
+        }
+
+        [HttpPost]
+        [Route("delete/{id}")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirm(int id, Issue issueModel)
+        {
+            this.context.Issues.Remove(issueModel);
+            this.context.SaveChanges();
+
+            return Redirect("/");
+        }
+    }
+}
